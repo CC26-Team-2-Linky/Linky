b1a53e8178a0ba989977108e8dee400aaf15feba
Thomas Ryan <DWTrip@gmail.com>
2022-07-14 18:17:22 +0900

    Feat: added pagination to backend under .../posts/page number

--- cc26-linky/src/components/App.jsx
+++ cc26-linky/src/components/App.jsx
@@ -6,18 +6,22 @@ import "../App.css";
 import { useEffect, useState } from "react";
 import ModalButton from "./ModalButton";
 
+const server = "http://localhost:9000";
+
 export default function App() {
   const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
 
   const fetchdata = async () => {
-    const response = await axios.get("https://cc26-linky.herokuapp.com/api");
+    const response = await axios.get(`${server}/api`);
     const info = response.data;
+    console.log(info);
     setData(info);
     console.log(data);
   };
 
   const postData = async (data) => {
-    await axios.post("https://cc26-linky.herokuapp.com/newpost", data);
+    await axios.post(`${server}/newpost`, data);
     fetchdata();
   };
 
