40162e3a53085b5906f7c877d877fb6dad3d846b
Thomas Ryan <DWTrip@gmail.com>
2022-07-15 16:36:35 +0900

    Fix: fixed conflict in routes/index.js. Pulled Yush's update.

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,19 +32,9 @@ router.get("/api", async (req, res) => {
 
 router.get("/posts/:page", async (req, res) => {
   const page = req.params.page;
-  console.log(`============Page is ${page}===================`);
   const dbData = await knex.select("*").from("posts");
-<<<<<<< HEAD
-  const sortArr = dbData.sort((a, b) => a["posted-at"] - b["posted-at"]);
-<<<<<<< HEAD
-  res.status(200).send(sortArr.slice(page * 2 - 2, page * 2));
-=======
   const sortArr = dbData.sort((a, b) => b["posted-at"] - a["posted-at"]);
   res.status(200).send(sortArr.slice(page * 10 - 10, page * 10));
->>>>>>> a12aea35411e01ebd4888fd61c107866adf7588e
-=======
-  res.status(200).send(sortArr.slice(page * 10 - 10, page * 10));
->>>>>>> cfdfeb5 (Fix: Updated variables for deployment)
 });
 
 router.get("/tags/:input", async (req, res) => {
